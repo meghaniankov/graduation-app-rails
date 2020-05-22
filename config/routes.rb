1760aed4b1793509638faf2d1c0a5974e90d4cbd
Meghan <meghan.e.mccue@gmail.com>
2020-05-22 18:26:50 +0100

    Add messages details

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
+  resources :messages
 
   root 'welcome#index'
 end
